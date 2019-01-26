08a419c8f7a27a2fa796ba1a310005d4f1843d9b
Mariya <mariya.ave@mail.ru>
2019-01-26 11:12:34 +0300

    step 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,14 +9,10 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  };
+  selectedVar: Hero;
+  onSelect(item: Hero): void {
+    this.selectedVar = item;
+  }
 
   constructor() { }
 
