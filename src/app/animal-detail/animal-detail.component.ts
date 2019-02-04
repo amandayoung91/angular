66ea6a745c74ada4fabb148233cdd3142dd4df46
Mariya <mariya.ave@mail.ru>
2019-02-04 18:13:58 +0300

    step 3 complete

--- src/app/animal-detail/animal-detail.component.ts
+++ src/app/animal-detail/animal-detail.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Animal } from './hero';
+import { Animal } from '../hero';
 
 @Component({
   selector: 'app-animal-detail',
   templateUrl: './animal-detail.component.html',
-  styleUrls: ['./animal-detail.component.css']
+  styleUrls: ['../heroes/heroes.component.css']
 })
 export class AnimalDetailComponent implements OnInit {
 
-  @Input() item2: Animal;
+  @Input() item3: Animal;
 
   constructor() { }
 
